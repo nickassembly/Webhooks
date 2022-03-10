eebeb9891354d9fc0fe0773dbdf03f04d9a61d67
nickguerra <nickg@maliciousgames.com>
2022-03-10 07:04:45 -0600

    Added db context for Airlineweb

--- AirlineWeb/Program.cs
+++ AirlineWeb/Program.cs
@@ -1,6 +1,12 @@
+using AirlineWeb.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<AirlineDbContext>(opt => 
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("AirlineConnection")));
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
