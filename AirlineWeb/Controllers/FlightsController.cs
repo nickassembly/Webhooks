4ca8c3b4c592c5d2bc7ef6931e5194a21778bcca
nickguerra <nickg@maliciousgames.com>
2022-03-17 07:13:48 -0500

    Added create and read endpoint for flights controller

---
+++ AirlineWeb/Controllers/FlightsController.cs
@@ -0,0 +1,65 @@
+using AirlineWeb.Data;
+using AirlineWeb.Dtos;
+using AirlineWeb.Models;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AirlineWeb.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FlightsController : ControllerBase
+    {
+        private readonly AirlineDbContext _context;
+        private readonly IMapper _mapper;
+        public FlightsController(AirlineDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        [HttpGet("{flightCode}", Name = "GetFlightDetailsByCode")]
+        public ActionResult<FlightDetailReadDto> GetFlightDetailsByCode(string flightCode)
+        {
+            var flight = _context.FlightDetails.FirstOrDefault(f => f.FlightCode == flightCode);
+
+            if (flight == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(_mapper.Map<FlightDetailReadDto>(flight));
+        }
+
+        [HttpPost]
+        public ActionResult<FlightDetailReadDto> CreateFlight(FlightDetailCreateDto flightDetailCreateDto)
+        {
+            var flight = _context.FlightDetails.FirstOrDefault(f => f.FlightCode == flightDetailCreateDto.FlightCode);
+
+            if (flight == null)
+            {
+                var flightDetailModel = _mapper.Map<FlightDetail>(flightDetailCreateDto);
+
+                try
+                {
+                    _context.FlightDetails.Add(flightDetailModel);
+                    _context.SaveChanges();
+                }
+                catch (Exception ex)
+                {
+                    return BadRequest(ex.Message);
+                }
+
+                var flightDetailReadDto = _mapper.Map<FlightDetailReadDto>(flightDetailModel);
+
+                return CreatedAtRoute(nameof(GetFlightDetailsByCode), new {flightCode = flightDetailReadDto.FlightCode}, flightDetailReadDto);
+
+            }
+            else
+            {
+                return NoContent();
+            }
+
+        }
+    }
+}
