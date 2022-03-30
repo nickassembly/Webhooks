ee2884814aade1959be448e6c5f4987e45d0d647
nickguerra <nickg@maliciousgames.com>
2022-03-30 07:19:12 -0500

    Added exchange for rabbitMq consumer

--- AirlineSendAgent/App/AppHost.cs
+++ AirlineSendAgent/App/AppHost.cs
@@ -1,10 +1,41 @@
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
 namespace AirlineSendAgent.App
 {
     public class AppHost : IAppHost
     {
         void IAppHost.Run()
         {
-            Console.WriteLine("Hello World");
+            var factory = new ConnectionFactory()
+            {
+                HostName = "localhost",
+                Port = 5672
+            };
+
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.ExchangeDeclare(exchange: "trigger", type: ExchangeType.Fanout);
+
+                var queueName = channel.QueueDeclare().QueueName;
+
+                channel.QueueBind(queue: queueName, exchange: "trigger", routingKey: "");
+
+                var consumer = new EventingBasicConsumer(channel);
+                Console.WriteLine("Listening on the message bus...");
+
+                consumer.Received += async (ModuleHandle, ea) =>
+                {
+                    Console.WriteLine("Event is Triggered");
+                };
+
+                channel.BasicConsume(queue: queueName, autoAck: true, consumer: consumer);
+
+                Console.ReadLine();
+                
+
+
+            }
         }
     }
 }
